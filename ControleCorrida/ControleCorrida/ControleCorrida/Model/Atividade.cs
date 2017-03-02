9c03a680f2097cfb5eefac1363f7a05525b23323
Pablo Tôndolo de Vargas <pablotdvsm@gmail.com>
2017-03-01 21:24:54 -0300

    Migração Xamarin.Forms e Azure

---
+++ ControleCorrida/ControleCorrida/ControleCorrida/Model/Atividade.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleCorrida.Model
+{
+    [DataTable("Atividade")]
+    public class Atividade
+    {
+        [JsonProperty("Id")]
+        public string Id { get; set; }
+
+        [JsonProperty("Data")]
+        public DateTime Data { get; set; }
+
+        [JsonProperty("Voltas")]
+        public decimal Voltas { get; set; }
+
+        [JsonProperty("Tempo")]
+        public DateTime Tempo { get; set; }
+
+        [Version]
+        public string Version { get; set; }
+
+        //public TimeSpan Media
+        //{
+        //    get
+        //    {
+        //        return new TimeSpan((long)(Tempo.Ticks / Voltas));
+        //    }
+        //}
+    }
+}
---
+++ ControleCorrida/ControleCorrida/ControleCorrida/ViewModel/AtividadesViewModel.cs
@@ -0,0 +1,81 @@
+﻿using ControleCorrida.Model;
+using ControleCorrida.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ControleCorrida.ViewModel
+{
+    public class AtividadesViewModel : ObservableBaseObject
+    {
+        public ObservableCollection<Atividade> Atividades { get; set; }
+        private AzureClient _client;
+        public Command RefreshCommand { get; set; }
+        public Command GenerateAtividadesCommand { get; set; }
+        public Command CleanLocalDataCommand { get; set; }
+
+        public bool IsBusy
+        {
+            get
+            {
+                return isBusy;
+            }
+
+            set
+            {
+                isBusy = value;
+                OnPropertyChanged();
+            }
+        }
+        private bool isBusy;
+
+        public AtividadesViewModel()
+        {
+            RefreshCommand = new Command(() => Load());
+            GenerateAtividadesCommand = new Command(() => generateAtividades());
+            CleanLocalDataCommand = new Command(() => cleanLocalData());
+            Atividades = new ObservableCollection<Atividade>();
+            _client = new AzureClient();
+        }
+
+        private async void cleanLocalData()
+        {
+            await _client.CleanData();
+        }
+
+        private void generateAtividades()
+        {
+            if (IsBusy)
+                return;
+
+            IsBusy = true;
+
+            _client.AddAtividade(new Atividade { Data = new DateTime(2017, 03, 01), Voltas = 09, Tempo = new DateTime(2017, 03, 01, 0, 31, 57) });
+            _client.AddAtividade(new Atividade { Data = new DateTime(2017, 02, 14), Voltas = 10, Tempo = new DateTime(2017, 02, 14, 0, 33, 22) });
+            _client.AddAtividade(new Atividade { Data = new DateTime(2017, 02, 13), Voltas = 10, Tempo = new DateTime(2017, 02, 13, 0, 32, 35) });
+            _client.AddAtividade(new Atividade { Data = new DateTime(2017, 02, 10), Voltas = 10, Tempo = new DateTime(2017, 02, 10, 0, 32, 27) });
+            _client.AddAtividade(new Atividade { Data = new DateTime(2017, 02, 09), Voltas = 10, Tempo = new DateTime(2017, 02, 09, 0, 32, 48) });
+            _client.AddAtividade(new Atividade { Data = new DateTime(2017, 02, 08), Voltas = 10, Tempo = new DateTime(2017, 02, 08, 0, 33, 21) });
+            _client.AddAtividade(new Atividade { Data = new DateTime(2017, 02, 07), Voltas = 10, Tempo = new DateTime(2017, 02, 07, 0, 33, 52) });
+
+            IsBusy = false;
+        }
+
+        public async void Load()
+        {
+            var result = await _client.GetAtividades();
+
+            Atividades.Clear();
+
+            foreach (var item in result)
+            {
+                Atividades.Add(item);
+            }
+            IsBusy = false;
+        }
+    }
+}
